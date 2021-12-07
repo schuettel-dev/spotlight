eab6e7cc0784bf1c7ae7b0db869fe75bb89ac750
mario <github@lxxxvi.ch>
2021-12-07 16:09:21 +0100

    add test for RevokeReqeustComponent

--- test/components/revoke_request_component_test.rb
+++ test/components/revoke_request_component_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class RevokeRequestComponentTest < ViewComponent::TestCase
-  test '#render' do
-    assert false, 'todo'
+  test 'render' do
+    travel_to '2001-01-04 10:00:00 +01:00' do
+      light_request = light_requests(:thursday_bart)
+      calendar_date = calendar_dates(:thursday)
+
+      component = new_component(calendar_date: calendar_date, user_light_request: light_request)
+
+      render_inline component
+
+      assert_text 'You have requested light for today.'
+      assert_button 'Revoke your light request.'
+    end
+  end
+
+  test 'not render, not awaiting light requests' do
+    travel_to '2001-01-04 16:35:00 +01:00' do
+      light_request = light_requests(:thursday_bart)
+      calendar_date = calendar_dates(:thursday)
+
+      component = new_component(calendar_date: calendar_date, user_light_request: light_request)
+
+      assert_not component.render?
+    end
+  end
+
+  test 'not render, light request not persisted' do
+    travel_to '2001-01-04 10:00:00 +01:00' do
+      light_request = users(:martin).light_request_for_today
+      calendar_date = calendar_dates(:thursday)
+
+      component = new_component(calendar_date: calendar_date, user_light_request: light_request)
+
+      assert_not component.render?
+    end
   end
 end
