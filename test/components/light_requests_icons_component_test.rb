715d26e26a0d434585ad840b45e4dd922c0b694e
mario <github@lxxxvi.ch>
2021-12-07 13:31:29 +0100

    add test for LightRequestsIconsComponent

--- test/components/light_requests_icons_component_test.rb
+++ test/components/light_requests_icons_component_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class LightRequestsIconsComponentTest < ViewComponent::TestCase
-  test '#render' do
-    assert false, 'todo'
+  test 'not render, 0 light request' do
+    component = new_component(light_requests: LightRequest.none)
+    assert_not component.render?
+  end
+
+  test 'render, 1 light request' do
+    component = new_component(light_requests: LightRequest.take(1))
+    render_inline component
+
+    assert_selector 'svg', count: 1
+  end
+
+  test 'render, multiple light request' do
+    component = new_component(light_requests: LightRequest.take(3))
+    render_inline component
+
+    assert_selector 'svg', count: 3
   end
 end
