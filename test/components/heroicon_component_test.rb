3f4094b8b98fb5d05d75a025868d1428927b9ff3
mario <github@lxxxvi.ch>
2021-12-09 09:14:32 +0100

    add test for HeroiconComponent

--- test/components/heroicon_component_test.rb
+++ test/components/heroicon_component_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class HeroiconComponentTest < ViewComponent::TestCase
-  test '#render' do
-    assert false, 'todo'
+  test 'render' do
+    render_inline(HeroiconComponent.new(:'dots-horizontal'))
+    assert_selector 'svg', count: 1
+  end
+
+  test 'raise if icon not exists' do
+    error = assert_raises(Errno::ENOENT) {
+      render_inline(HeroiconComponent.new(:'whoops'))
+    }
+
+    assert_match /^No such file or directory/, error.message
   end
 end
