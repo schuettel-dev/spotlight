a6dc81f647dd9d05eda110094c8718554e5d96d6
mario <github@lxxxvi.ch>
2021-12-09 11:28:52 +0100

    add test for TitleComponent

--- test/components/title_component_test.rb
+++ test/components/title_component_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class TitleComponentTest < ViewComponent::TestCase
   test '#render' do
-    assert false, 'todo'
+    render_inline(new_component)
+    assert_text 'SPOTLIGHT'
+    assert_text 'at Skatepark Schachen, Aarau'
   end
 end
