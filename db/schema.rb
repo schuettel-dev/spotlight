4d584ad89e6164e38002d7e8b4d64dfa587465b5
mario <github@lxxxvi.ch>
2022-02-23 13:34:30 +0100

    upgrade Ruby and gems, various fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_090248) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_11_23_090248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "calendar_dates", force: :cascade do |t|
     t.date "date", null: false
     t.boolean "active", default: true, null: false
-    t.datetime "request_window_starts_at", precision: 6, null: false
-    t.datetime "request_window_ends_at", precision: 6, null: false
-    t.datetime "caretaker_informed_at", precision: 6
-    t.datetime "caretaker_confirmed_light_at", precision: 6
-    t.datetime "caretaker_dismissed_light_at", precision: 6
-    t.datetime "sun_sets_at", precision: 6, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "request_window_starts_at", null: false
+    t.datetime "request_window_ends_at", null: false
+    t.datetime "caretaker_informed_at"
+    t.datetime "caretaker_confirmed_light_at"
+    t.datetime "caretaker_dismissed_light_at"
+    t.datetime "sun_sets_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["date"], name: "index_calendar_dates_on_date", unique: true
   end
 
   create_table "configurations", force: :cascade do |t|
     t.string "key", null: false
     t.json "value_json", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["key"], name: "index_configurations_on_key", unique: true
   end
 
   create_table "light_requests", force: :cascade do |t|
     t.bigint "calendar_date_id", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["calendar_date_id"], name: "index_light_requests_on_calendar_date_id"
     t.index ["user_id"], name: "index_light_requests_on_user_id"
   end
@@ -55,10 +54,10 @@ ActiveRecord::Schema.define(version: 2021_11_23_090248) do
     t.string "role", default: "user", null: false
     t.string "status", default: "unverified", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -69,8 +68,8 @@ ActiveRecord::Schema.define(version: 2021_11_23_090248) do
     t.boolean "active", default: true, null: false
     t.time "request_window_starts_at", null: false
     t.time "request_window_ends_at", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["weekday"], name: "index_weekday_templates_on_weekday", unique: true
   end
 
